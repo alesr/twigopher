d7c79a1a47aed94e0da50854641bddf4a2af76d1
Alessandro Resta <alessandro.resta@gmail.com>
2017-04-03 17:05:28 +0100

    Call account.Stat method in main.go

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/alesr/twigopher/account"
 	"github.com/alesr/twigopher/client"
 )
 
 func main() {
 
-	_, err := client.NewClient()
+	client, err := client.NewClient()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("%v\n\n", account.Stat(client))
 }
