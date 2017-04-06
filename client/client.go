774d17d4b6ccbbbb454719d1bc8367d5323d1fb8
Alessandro Resta <alessandro.resta@gmail.com>
2017-04-06 13:52:42 +0100

    Client polishing and refactoring

--- client/client.go
+++ client/client.go
@@ -3,6 +3,8 @@ package client
 import (
 	"errors"
 
+	"fmt"
+
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 	"github.com/kurrik/oauth1a"
@@ -22,6 +24,12 @@ type Application struct {
 	accessSecret string
 }
 
+// Credentials wraps consumer and application credentials
+type Credentials struct {
+	Consumer    *Consumer
+	Application *Application
+}
+
 // Client wraps twittergo.Client for
 // getting information on credential usage
 // and twitter.StreamService to stream twitter data
@@ -35,16 +43,7 @@ type Client struct {
 func NewClient() (*Client, error) {
 
 	// Load configuration file
-	if err := loadConfig(); err != nil {
-		return nil, err
-	}
-
-	c, err := newConsumer()
-	if err != nil {
-		return nil, err
-	}
-
-	a, err := newApplication()
+	credentials, err := loadConfig("./twitter.yaml")
 	if err != nil {
 		return nil, err
 	}
@@ -54,14 +53,14 @@ func NewClient() (*Client, error) {
 	// The next step would be to isolate the functionalities we want to keep
 	// and create a clean client
 	config := &oauth1a.ClientConfig{
-		ConsumerKey:    c.key,
-		ConsumerSecret: c.secret,
+		ConsumerKey:    credentials.Consumer.key,
+		ConsumerSecret: credentials.Consumer.secret,
 	}
 
-	user := oauth1a.NewAuthorizedConfig(a.accessToken, a.accessSecret)
-	token := oauth1.NewToken(a.accessToken, a.accessSecret)
+	user := oauth1a.NewAuthorizedConfig(credentials.Application.accessToken, credentials.Application.accessSecret)
+	token := oauth1.NewToken(credentials.Application.accessToken, credentials.Application.accessSecret)
 
-	httpClient := oauth1.NewConfig(c.key, c.secret).Client(oauth1.NoContext, token)
+	httpClient := oauth1.NewConfig(credentials.Consumer.key, credentials.Consumer.secret).Client(oauth1.NoContext, token)
 
 	return &Client{
 		twittergo.NewClient(config, user),
@@ -69,50 +68,80 @@ func NewClient() (*Client, error) {
 	}, nil
 }
 
-// newConsumer returns the Consumer credentials
-func newConsumer() (*Consumer, error) {
+// loadConfig values from config.yaml
+func loadConfig(filepath string) (*Credentials, error) {
 
-	if !viper.IsSet("consumer_key") {
-		return nil, errors.New("could not load consumer key")
+	viper.SetConfigFile(filepath)
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, errors.New("no configuration file loaded")
 	}
 
-	if !viper.IsSet("consumer_secret") {
-		return nil, errors.New("could not load access consumer secret")
+	consumer, err := consumer()
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve consumer credentials: %s", err)
 	}
 
-	return &Consumer{
-		viper.GetString("consumer_key"),
-		viper.GetString("consumer_secret"),
-	}, nil
+	application, err := application()
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve application credentials: %s", err)
+	}
+
+	credentials := &Credentials{
+		Consumer:    consumer,
+		Application: application,
+	}
+
+	// We don't need Viper anymore since our credentials are already load
+	viper.Reset()
+
+	return credentials, nil
+}
+
+// consumer returns the Consumer credentials
+func consumer() (*Consumer, error) {
+
+	keys := []string{
+		"consumer_key",
+		"consumer_secret",
+	}
+
+	c := &Consumer{}
+
+	for _, k := range keys {
+		if !viper.IsSet(k) {
+			return nil, fmt.Errorf("could not load: %s", k)
+		}
+
+		if k == "consumer_key" {
+			c.key = viper.GetString("consumer_key")
+		} else {
+			c.secret = viper.GetString("consumer_secret")
+		}
+	}
+	return c, nil
 }
 
 // newApplication returns the Application credentials
-func newApplication() (*Application, error) {
+func application() (*Application, error) {
 
-	if !viper.IsSet("access_token") {
-		return nil, errors.New("could not load access token")
+	keys := []string{
+		"access_token",
+		"access_token_secret",
 	}
 
-	if !viper.IsSet("access_token_secret") {
-		return nil, errors.New("could not load access token secret")
+	a := &Application{}
+
+	for _, k := range keys {
+		if !viper.IsSet(k) {
+			return nil, fmt.Errorf("could not load: %s", k)
+		}
+
+		if k == "access_token" {
+			a.accessToken = viper.GetString("access_token")
+		} else {
+			a.accessSecret = viper.GetString("access_token_secret")
+		}
 	}
-
-	return &Application{
-		viper.GetString("access_token"),
-		viper.GetString("access_token_secret"),
-	}, nil
-}
-
-// loadConfig values from config.yaml
-func loadConfig() error {
-
-	viper.SetConfigName("twitter")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		return errors.New("no configuration file loaded")
-	}
-	return nil
+	return a, nil
 }
